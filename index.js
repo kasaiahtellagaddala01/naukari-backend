f57c4a3cb3d4a2eda2ed026a455330fb8b388d16
kasaiahtellagaddala01 <kasaiahtellagaddala@gmail.com>
2025-04-04 19:01:52 +0530

    added index.js

--- index.js
+++ index.js
@@ -6,24 +6,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('your_mongo_url_here', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://naukariuser:Kasi3056@cluster0.jwmykfq.mongodb.net/naukariDB?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const jobSchema = new mongoose.Schema({
   title: String,
   description: String,
   postedAt: { type: Date, default: Date.now }
 });
+
 const Job = mongoose.model('Job', jobSchema);
 
+// GET all jobs
 app.get('/api/jobs', async (req, res) => {
   const jobs = await Job.find().sort({ postedAt: -1 });
   res.json(jobs);
 });
 
+// POST new job
 app.post('/api/jobs', async (req, res) => {
   const newJob = new Job(req.body);
   await newJob.save();
   res.status(201).json(newJob);
 });
 
-app.listen(3000, () => console.log('Server running'));
+// Start server
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
